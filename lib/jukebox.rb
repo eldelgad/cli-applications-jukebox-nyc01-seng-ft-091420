550ddcfa460d1e848f89ff2641706ed2688f25a5
Elias Delgado <evdelgado99@gmail.com>
2020-09-14 03:02:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,6 +55,22 @@ end
 
 def run
   
-  puts "Please enter a command:"
+while true do
+    puts "Please enter a command:"
+    response = gets.chomp
+    case response
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
+    end
+  end
   
 end
