724993d9441d687c34a7ecb0b07d741a0e231601
Elias Delgado <evdelgado99@gmail.com>
2020-09-14 02:51:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,10 +27,10 @@ def play(songs)
   puts "Please enter a song name or number:"
   song_name = gets.strip
   # binding.pry
-  if song_name != songs
-    puts "Invalid input, please try again"
-  else 
+  if song_name = songs
     puts "Playing #{songs}"
+  else song_name != songs
+    puts "Invalid input, please try again"
   end
   
 end
