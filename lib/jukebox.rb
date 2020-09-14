2e264034c72ad99c38c353ceeb00384a35e2ce53
Elias Delgado <evdelgado99@gmail.com>
2020-09-14 03:14:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -57,21 +57,20 @@ def run
   
 while true do
     puts "Please enter a command:"
-    user_input = gets.chomp
-    
-    case user_input
-      when "exit"
-        exit_jukebox
-        break
-      when "play"
-        play(songs)
-      when "help"
-        help
-      when "list"
-        list(songs)
-      else
-        puts "Invalid entry"
-      end
+    response = gets.chomp
+    case response
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
     end
+  end
   
 end
