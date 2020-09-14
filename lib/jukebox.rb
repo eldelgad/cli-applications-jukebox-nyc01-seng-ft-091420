7edf3a996e5b10b74817667e4a64f456e2252ba1
Elias Delgado <evdelgado99@gmail.com>
2020-09-14 02:52:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ def play(songs)
   # binding.pry
   if song_name = songs
     puts "Playing #{songs}"
-  elsif song_name > 8
+  elsif song_name != songs
     puts "Invalid input, please try again"
   end
   
